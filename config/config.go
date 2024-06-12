fe1899ea0d6197c79798ad996d6ac28ea83d8e80
GevorgGal <100610853+GevorgGal@users.noreply.github.com>
2024-06-12 12:05:50 -0700

    Add logic to load environment variables

--- config/config.go
+++ config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func GetAPIKey() string {
+	apiKey := os.Getenv("OPENWEATHER_API_KEY")
+	if apiKey == "" {
+		log.Fatal("No API key found")
+	}
+	return apiKey
+}
