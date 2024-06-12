b0891396f5351b90a7215b7d5fbed20f1cdd439d
GevorgGal <100610853+GevorgGal@users.noreply.github.com>
2024-06-12 12:51:37 -0700

    add logic to main file to start server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/GevorgGal/weather-server/internal/handlers"
+)
+
+func main() {
+	http.HandleFunc("/weather", handlers.WeatherHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Starting server on port %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
